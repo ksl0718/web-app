5aef8c3f283364203742dc25c49dd8fe42d445fd
ksl0718 <ksl0718@nate.com>
2017-09-12 14:36:51 +0900

    sda

---
+++ zz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	int i, j;
+	for(int i = 0 ; i<6; ++i)
+	{
+		for(j=0; j<i+1;j++){
+			printf("ㅋ");
+		}
+		if(i==6){
+		}
+		printf("\n");
+	}
+	printf("ㅋ\n");
+}
+
